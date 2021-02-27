a2d347008ff8419e0e78948e50b83f8482c1be09
Cole Flournoy <flournoy.cole@gmail.com>
2021-02-27 16:11:58 -0500

    commit tree 037dd11000b90cf27dcc2a527a42eb0251ab2320

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :post_must_be_clickbait
+
+  def post_must_be_clickbait
+    if title.present? && !title[/Won't Believe|Secret|Top|Guess/]
+      errors.add(:title, "must be clickbaity")
+    end
+  end
 end
